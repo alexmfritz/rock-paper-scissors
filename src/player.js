8f6508d6c688be555165db111401d3867fbfe97d
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-10 12:22:05 -0800

    Create Player class

---
+++ src/player.js
@@ -0,0 +1,18 @@
+
+class Player {
+    constructor(name, emoji) {
+        this.name = name;
+        this.emoji = emoji;
+        this.wins = 0;
+        this.choice;
+    }
+    takeTurn() {
+
+    }
+    saveWinsToStorage() {
+
+    }
+    retrieveWinsFromStorage() {
+
+    }
+}
