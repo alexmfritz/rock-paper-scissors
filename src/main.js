d6a767e3276612632e46144d1099405d5d437cd3
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-10 14:06:47 -0800

    Create display functions and event listeners to allow correct choices to display on click

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 var classicChoiceImages = document.querySelector('.classic');
 var spicyChoiceImages = document.querySelector('.spicy');
 // classic inputs
-var ironManClassic = document.getElementsById('ironManClassic');
+var ironManClassic = document.getElementById('ironManClassic');
 var capAmClassic = document.getElementById('capAmClassic');
 var thorClassic = document.getElementById('thorClassic');
 // spicy inputs
@@ -12,7 +12,7 @@ var thorSpicy = document.getElementById('thorSpicy');
 var hulkSpicy = document.getElementById('hulkSpicy');
 var widowSpicy = document.getElementById('widowSpicy');
 // buttons
-var classicButton = documewnt.querySelector('.classic-button');
+var classicButton = document.querySelector('.classic-button');
 var spicyButton = document.querySelector('.spicy-button');
 var changeGameButton = document.querySelector('.game-button');
 // player
@@ -28,10 +28,24 @@ var winnerAnouncement = document.getElementById('winnerAnnounce');
 // data model
 var game;
 
+// event listeners
 
+classicButton.addEventListener('click', displayClassic);
+spicyButton.addEventListener('click', displaySpicy);
 
+// event handlers
 
+function displayClassic(event) {
+    show(classicChoiceImages);
+    hide(classicButton);
+    hide(spicyButton);
+};
 
+function displaySpicy(event) {
+    show(spicyChoiceImages);
+    hide(classicButton);
+    hide(spicyButton);
+};
 
 // helper functions
 
