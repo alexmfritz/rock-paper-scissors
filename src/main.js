1504adb436b9bbc8884d61efd4744f5cfec2fbc8
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-10 12:52:15 -0800

    Create initial DOM variables for the user choice images for both classic and spicy

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// images
+var classicChoiceImages = document.querySelector('.classic');
+var spicyChoiceImages = document.querySelector('.spicy');
+// classic inputs
+var ironManClassic = document.getElementsById('ironManClassic');
+var capAmClassic = document.getElementById('capAmClassic');
+var thorClassic = document.getElementById('thorClassic');
+// spicy inputs
+var ironManSpicy = document.getElementById('ironManSpicy');
+var capAmSpicy = document.getElementById('capAmSpicy');
+var thorSpicy = document.getElementById('thorSpicy');
+var hulkSpicy = document.getElementById('hulkSpicy');
+var widowSpicy = document.getElementById('widowSpicy');
