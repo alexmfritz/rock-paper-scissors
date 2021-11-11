34ef6d05e8fe74d327ac02165e517de82eaaee5f
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-10 17:27:48 -0800

    Build out Player class method to take turn or make a choice

--- src/player.js
+++ src/player.js
@@ -6,8 +6,11 @@ class Player {
         this.wins = 0;
         this.choice;
     }
-    takeTurn() {
-
+    takeTurn(decision) {
+        if (!decision) {
+            this.choice.getRandomIndex(this.choice);
+        }
+        this.choice = decision;
     }
     saveWinsToStorage() {
 
