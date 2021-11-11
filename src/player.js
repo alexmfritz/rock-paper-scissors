dfde9b232c81d757537d90d34c596cd7dad630fc
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-10 20:04:26 -0800

    Continue to build out Player class

--- src/player.js
+++ src/player.js
@@ -13,9 +13,14 @@ class Player {
         this.choice = decision;
     }
     saveWinsToStorage() {
-
+        var storedWins = this.wins;
+        var stringifiedWins = JSON.stringify(storedWins);
+        localStorage.setItem(`${this.name} wins`, stringifiedWins);
     }
     retrieveWinsFromStorage() {
-
+        var retrievedWins = localStorage.getItem(`${this.name} wins`);
+        var parsedWins = JSON.parse(retrievedWins);
+        this.wins = parsedWins;
+        return `Wins: ${this.wins}`;
     }
 }
