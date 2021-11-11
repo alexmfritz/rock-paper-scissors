a2b55a4c3e85d19eed98c670274c1c07d9c55768
Alex Fritz <alexfritzcoach@gmail.comm>
2021-11-11 14:40:01 -0800

    Add return statements in method logic in order to change text based on results

--- src/game.js
+++ src/game.js
@@ -16,23 +16,30 @@ class Game {
     determineWinner() {
         if (this.player.choice === this.player.choice) {
             this.winner = 'Draw';
+            return 'It\s a draw!';
         } else if ((this.player.choice === 'iron man') && (this.computer.choice === 'captain america') || (this.player.choice === 'iron man') && (this.computer.choice === 'the hulk')) {
             this.player.wins++;
             this.winner = this.player.name;
+            return  `${game.player.name} wins!`;
         } else if ((this.player.choice === 'captain america') && (this.computer.choice === 'thor') || (this.player.choice === 'captain america') && (this.computer.choice === 'black widow')) {
             this.player.wins++;
             this.winner = this.player.name;
+            return  `${game.player.name} wins!`;
         } else if ((this.player.choice === 'thor') && (this.computer.choice === 'iron man') || (this.player.choice === "thor") && (this.computer.choice === 'black widow')) {
             this.player.wins++;
             this.winner = this.player.name;
+            return  `${game.player.name} wins!`;
         } else if ((this.player.choice === 'the hulk') && (this.computer.choice === 'thor') || (this.player.choice === 'the hulk') && (this.computer.choice === 'captain america')) {
             this.player.wins++;
             this.winner = this.player.name;
+            return  `${game.player.name} wins!`;
         } else if ((this.player.choice === 'black widow') && (this.computer.choice === 'iron man') || (this.player.choice === 'black widow') && (this.computer.choice === 'the hulk')) {
             this.player.wins++;
             this.winner = this.player.name;
+            return  `${game.player.name} wins!`;
         } 
         this.computer.wins++;
         this.winner = this.computer.name;
+        return  `${game.computer.name} wins!`;
     }
 }
